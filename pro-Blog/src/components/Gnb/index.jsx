9a146f516dcbf6d8ba7a21be9498b37783ceb413
ravenusric <richie.roney@gmail.com>
2018-08-23 12:14:19 -0400

    updated

--- pro-Blog/src/components/Gnb/index.jsx
+++ pro-Blog/src/components/Gnb/index.jsx
@@ -52,7 +52,7 @@ const GnbWrapper = styled.div`
   position: fixed;
   width: 100%;
   height: 100px;
-  line-height: 100px;
+  line-height: 120px;
   background-color: rgba(255, 255, 255, .6);
   font-size: 18px;
   font-family: Lato;
